559e029d6b4587d7fd3cc64b0003590c183377e6
KhushSanghavi <sanghavikhush2003@gmail.com>
2024-03-07 14:02:07 +0530

    project completed

---
+++ frontend/src/components/Image.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function Image(props) {
+        
+
+        return (
+                <>
+                 <div className='w-24 h-24 object-center ml-40 mt-10 pt-50'>
+                <img src='/pay.png' className='rounded-2xl'/>
+                </div>       
+                </>
+        )
+}
